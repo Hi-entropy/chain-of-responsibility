4b1283628036bf6261d284f9a2801efb5d3318a1
entropy <hientropia@proton.me>
2025-09-23 23:56:29 -0300

    Move validations rules to vector

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,15 @@ mod validator;
 use crate::rule::ValidationRule;
 
 fn main() {
-    let rule1 = ValidationRule::new(
-        Box::new(|x: &i32| *x > 10),
-        "Number should be greater than 10".to_string()
-    );
-
-    let rule2 = ValidationRule::new(
-        Box::new(|x: &i32| *x > 25),
-        "Number should be less than 25".to_string()
-    );
-
     let rules = vec![
-        rule1,
-        rule2
+        ValidationRule::new(
+            Box::new(|x: &i32| *x > 10),
+            "Value should be greater than 10".to_string()
+        ),
+        ValidationRule::new(
+            Box::new(|x: &i32| *x < 25),
+            "Value should be less than 25".to_string()
+        )
     ];
     let validated = validator::Validator::validate(12, rules);
 
