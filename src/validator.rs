1caecebdee1232b42457a54ea297310ea7e51737
entropy <hientropia@proton.me>
2025-09-24 00:02:58 -0300

    Add valid value flow test

--- src/validator.rs
+++ src/validator.rs
@@ -13,3 +13,27 @@ impl Validator {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod validator_test {
+    use super::*;
+
+    #[test]
+    fn validates_correct_primitive_value() {
+        let rules = vec![
+            ValidationRule::new(
+                Box::new(|x: &i32| *x > 0),
+                "Value should be greater than 0".to_string()
+            ),
+            ValidationRule::new(
+                Box::new(|x: &i32| *x < 100),
+                "Value should be less than 100".to_string()
+            )
+        ];
+
+        let value = 30;
+        let result = Validator::validate(value, rules);
+
+        assert!(result.is_ok());
+    }
+}
